45a609b54552b41ca533a6b0ea08471f8fe3707e
Bryson Phillip <bkphillip@icloud.com>
2022-09-23 18:04:19 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -18,22 +18,40 @@ from fonts import FONTS
 
 text_color = (0, 0, 0)
 image = "image_placeholder.jpeg"
+img_width, img_height = 600, 600
+
+
+def rgb_to_hex(rgb):
+    return "#%02x%02x%02x" % rgb
 
 
 def change_text_color():
     colors = askcolor(title="Watermark Color Chooser")
     global text_color
     text_color = colors[0]
+    color_selection.configure(bg=colors[1])
 
 
 def apply_watermark():
-    watermark_image = Image.open(image)
-    my_font = ImageFont.truetype(font_var.get(), current_value.get())
-    edited_image = ImageDraw.Draw(watermark_image)
-    edited_image.text((28, 36), watermark_input.get(), font=my_font, fill=text_color)
+    watermark_image = Image.open(image).convert('RGBA')
+    text_image = Image.new('RGBA', watermark_image.size, (255, 255, 255, 0))
 
-    # Save image
-    watermark_image.save("new_img_with_watermark.jpeg")
+    my_font = ImageFont.truetype(font_var.get(), current_font_size.get())
+    my_watermark = ImageDraw.Draw(text_image)
+
+    opacity_tuple = ((round(current_opacity.get() * 2.55)),)
+    rgba_text_color = text_color + opacity_tuple
+
+    # Setup Watermark
+    my_watermark.text((width_position.get(), height_position.get()),
+           watermark_input.get(),
+           font=my_font,
+           fill=rgba_text_color
+           )
+
+    # Combine image with text and save image
+    combined = Image.alpha_composite(watermark_image, text_image).convert('RGB')
+    combined.save("new_img_with_watermark.jpeg", format='JPEG')
 
     # Apply changes to screen
     updated_image = ImageTk.PhotoImage(Image.open("new_img_with_watermark.jpeg"))
@@ -42,8 +60,11 @@ def apply_watermark():
 
 
 def file_open():
-    global image
+    global image, img_width, img_height
     image = askopenfilename()   # Add filetypes
+    img_width, img_height = Image.open(image).size
+    width_position.config(to=img_width)
+    height_position.config(to=img_height)
     uploaded_image = ImageTk.PhotoImage(Image.open(image))
     label.configure(image=uploaded_image)
     label.image = uploaded_image
@@ -92,27 +113,42 @@ font_var = StringVar()
 drop_menu = OptionMenu(edit_frame, font_var, FONTS[0], *FONTS)
 drop_menu.grid(column=1, row=2)
 
+
 # font size
-current_value = IntVar()
+current_font_size = IntVar(value=30)
 font_size_label = Label(edit_frame, text="Fontsize: ")
 font_size_label.grid(column=0, row=3)
-font_size = Spinbox(edit_frame, from_=8, to=75, textvariable=current_value, wrap=True)
+font_size = Spinbox(edit_frame, from_=8, to=150, wrap=True, textvariable=current_font_size)
 font_size.grid(column=1, row=3)
-
+# Opacity
+current_opacity = IntVar(value=100)
+opacity_label = Label(edit_frame, text="Opacity")
+opacity_label.grid(column=0, row=4)
+opacity_spinbox = Spinbox(edit_frame, from_=0, to=100, textvariable=current_opacity, wrap=False)
+opacity_spinbox.grid(column=1, row=4)
 # Text color
+color_selection = Canvas(edit_frame, bg=rgb_to_hex(text_color), width=15, height=15)
+color_selection.grid(column=0, row=5)
 color_button = Button(edit_frame, text="Text Color", command=change_text_color)
-color_button.grid(column=0, row=4, columnspan=2)
-
+color_button.grid(column=1, row=5, columnspan=1)
 # Position
+height_label = Label(edit_frame, text="Height")
+height_label.grid(column=0, row=6)
+height_position = Scale(edit_frame, from_=0, to=img_height, orient=VERTICAL)
+height_position.grid(column=1, row=6)
+width_label = Label(edit_frame, text="Width")
+width_label.grid(column=0, row=7)
+width_position = Scale(edit_frame, from_=0, to=img_width, orient=HORIZONTAL)
+width_position.grid(column=1, row=7)
 
 # Angle
 
 
 # Apply Button
 confirm_button = Button(edit_frame, text="Add Watermark", command=apply_watermark)
-confirm_button.grid(column=0, row=5, columnspan=2)
+confirm_button.grid(column=0, row=8, columnspan=2)
 
 # save_button
 save_button = Button(edit_frame, text="Save", command=save_new_image)
-save_button.grid(column=0, row=6, columnspan=2)
+save_button.grid(column=0, row=9, columnspan=2)
 window.mainloop()
